c6e30dfb786bc7254802efba52017225edb2f9da
Brian DeRocher <brian@ten>
2016-10-03 23:14:46 -0400

    Start splitting tabblocks into parts based on district boundaries.

--- make_poly.sql
+++ make_poly.sql
@@ -1,3 +1,46 @@
+set search_path to acps_redistricting, alexandria, census_2010, public;
+
+
+select addgeometrycolumn( 'alexandria', 'bld_y', 'center', '4326', 'POINT', 2 );
+update bld_y set center = st_centroid( wkb_geometry );
+create index idx_bld_y_center on bld_y using gist(center);
+vacuum analyze bld_y;
+
+
+
+
+--shp2pgsql -s 4326 -c -g geom -I tabblock2010_51_pophu.shp census_2010.tabblock | psql gis
+
+create index idx_tabblock_countyfp10 on tabblock(countyfp10);
+cluster tabblock using idx_tabblock_countyfp10;
+alter table tabblock add column area float;
+update tabblock set area = st_area(geom::geography);
+alter table tabblock alter column area set not null;
+
+alter table tabblock add column density float;
+-- non have area = 0
+update tabblock set density = pop10/area;
+alter table tabblock alter column density set not null;
+
+
+
+ -- Any overlaps?
+select *
+from
+	(
+	select *
+	from census_2010.tabblock
+	where countyfp10='510'
+	) a
+join
+	(
+	select *
+	from census_2010.tabblock
+	where countyfp10='510'
+	) b on st_overlaps(a.geom, b.geom);
+-- no
+ 
+
 drop view if exists make_district_poly;
 
 create view make_district_poly as 
@@ -28,14 +71,24 @@ alter table acps_district add primary key (id);
 create index idx_acps_district_geom on acps_district using gist(geom);
 
 
+-- Any overlaps?
+select *
+from acps_district a
+join acps_district b on st_overlaps(a.geom,b.geom);
+-- no
+
+
 
 drop table if exists bld_district;
+-- TODO: Use centroid of building to avoid loosing buildings that are
+-- not fully in a single district.  Or fix district boundarys to not
+-- slice buildings.
 create table bld_district as
 select b.objectid as bld, d.id as district
 from alexandria.bld_y b
 join acps_district d on st_within( b.wkb_geometry, d.geom )
-where b.buse = 1
-;
+where b.buse = 1;
+
 
 select district, count(*) from bld_district group by district;
 select bld, count(*) from bld_district group by bld having count(*) > 1;
@@ -57,4 +110,76 @@ from bld_district bd
 join bld_unit using (bld);
 
 select district, count(*) from bld_unit_district group by district;
+
+
+
+
+
+
+-- Some districts clearly cut blocks into parts.
+
+create sequence seq_district_block_id;
+
+-- Merge the acps_districta and tabblock layers
+drop table if exists district_block;
+create table district_block as
+select nextval('seq_district_block_id') as id, name, gid, st_multi(geom) as geom, type
+from (
+	select name, gid, (st_dump(st_intersection(d.geom, b.geom))).geom as geom, 'i'::char as type
+	from (select * from acps_district where year = 2016) d
+	join (select * from tabblock where countyfp10 = '510') b
+	 	on st_intersects(d.geom, b.geom)
+	) s
+where st_geometrytype(geom) = 'ST_Polygon';
+
+insert into district_block
+select nextval('seq_district_block_id'), name, null, st_multi(st_difference(d.geom, b.geom)) as geom, 'd'
+from (select * from acps_district where year = 2016) d
+join (select st_union(geom) as geom from tabblock where countyfp10 = '510') b
+	on st_overlaps(d.geom, b.geom);
+
+insert into district_block
+select nextval('seq_district_block_id'), null, gid, st_multi(st_difference(b.geom, d.geom)) as geom, 'b'
+from (select st_union(geom) as geom from acps_district where year = 2016) d
+join (select * from tabblock where countyfp10 = '510') b
+	on st_overlaps(d.geom, b.geom);
+
+
+insert into geometry_columns
+values ('', 'acps_redistricting', 'district_block', 'geom', 2, 4326, 'MULTIPOLYGON');
+
+alter table district_block add primary key (id);
+--create unique index idx_district_block_name_gid on district_block (name, gid);
+select name, gid, count(*)
+from district_block
+group by name, gid
+having 1 < count(*);
+-- whoa, 92 rows
+
+
+
+-- For each district_block, what percentage of people live in it.
+-- Divide the population by number of units in that block.
+alter table district_block add column units integer;
+
+-- Find the number of units in each district_block.
+update district_block db
+set units = sel.units
+from (
+	select db.id, sum(bunits) as units
+	from district_block db
+	join bld_y bld on st_within( center, db.geom )
+	group by db.id
+	) sel
+where db.id = sel.id;
+
+
+select count(*) from district_block;
+-- TODO: verify this number
+
+
+
+
+
+
 
